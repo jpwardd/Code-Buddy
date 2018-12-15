7dc5a7d8b806a4b7d8cd2889101e410d6a7b70ed
jpwardd <jward9421@gmail.com>
2018-12-15 10:40:09 -0500

    server side validations checkpoint

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import { hash } from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: email => User.dontExist({ email }),
+      message: ({ value }) => `Email ${value} has already been taken.` //TODO security
+    }
+  },
+  username: {
+    type: String,
+    validate: {
+      validator: username => User.dontExist({ username }),
+      message: ({ value }) => `Username ${value} has already been taken.` //TODO security
+    }
+  },
+  name: String,
+  password: String
+}, {
+  timestamps: true
+})
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await hash(this.password, 10)
+  }
+})
+
+userSchema.statics.dontExist = async function (options) {
+  return await this.where(options).countDocuments() === 0
+}
+
+const User = mongoose.model('User', userSchema)
+
+export default User
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,15 +1,28 @@
+import Joi from 'joi'
+import mongoose from 'mongoose'
+import { UserInputError } from 'apollo-server-express'
+import { SignUp } from '../schemas'
+import { User } from '../models'
+
 export default {
   Query: {
-    users: (root, arg, context, info) => {
-      
+    users: (root, args, context, info) => {
+      // TODO: suth, projection, pagination
+      return User.find({})
     },
-    user: (root, arg, context, info) => {
-
+    user: (root, { id }, context, info) => {
+      if (mongoose.Types.ObjectId.isValid(id)) {
+        throw new UserInputError(`${id} is not a valid user ID.`)
+      }
+      return User.findById(id)
     }
   },
   Mutation: {
-     signUp: (root, arg, context, info) => {
-       
-     }
+    signUp: async (root, args, context, info) => {
+      // TODO: not auth 
+      await Joi.validate(args, SignUp, { abortEarly: false })
+      // validation
+      return User.create(args)
+    }
   }
 }
